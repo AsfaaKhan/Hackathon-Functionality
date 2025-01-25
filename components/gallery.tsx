3ea6cb54d949869942768ba942e2e72d2f059f46
AsfaaKhan <khanzadiasfi0314@gmail.com>
2025-01-25 14:56:36 +0500

    add to cart functionality

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -27,7 +27,7 @@ export default  function Gallery() {
             <div className="container flex flex-col  gap-[30px]  lg:flex-row">
                 {/* 1st side */}
 
-                <div className=" w-full lg:w-[500px] h-[500px]  bg-[url('/sanity/1.png')] bg-no-repeat bg-cover bg-center ">
+                <div className=" w-full lg:w-[500px] h-[500px]  bg-[url('/sanity/1-min.png')] bg-no-repeat bg-cover bg-center ">
                     <div className=" pt-[400px] pl-[14px]">
                         
                         <Button variant={"categoryButton"} onClick={() => handleClick("cozy-sets")} className="hover:bg-slate-200" >Cozy Sets</Button>
@@ -40,7 +40,7 @@ export default  function Gallery() {
 
                     {/* Image1 */}
                     <div className="flex-1">
-                        <div className=" h-[500px] w-full lg:w-[240px] sm:h-[500px]  bg-[url('/sanity/2.png')] bg-no-repeat bg-cover bg-center ">
+                        <div className=" h-[500px] w-full lg:w-[240px] sm:h-[500px]  bg-[url('/sanity/2-min.png')] bg-no-repeat bg-cover bg-center ">
                             <div className=" pt-[400px] pl-[14px]">
                                 <Button variant={"categoryButton"}  onClick={() => handleClick("furniture")} className="hover:bg-slate-200" >Furniture</Button>
                                 
@@ -53,7 +53,7 @@ export default  function Gallery() {
 
                         {/* 3rd Image */}
 
-                        <div className="w-full  lg:w-[240px]   h-[240px] bg-[url('/sanity/3.png')] bg-no-repeat bg-cover bg-center ">
+                        <div className="w-full  lg:w-[240px]   h-[240px] bg-[url('/sanity/3-min.png')] bg-no-repeat bg-cover bg-center ">
                             <div className="mt-[171px] ml-[14px]">
                                 
                                 <Button  variant={"categoryButton"} onClick={() => handleClick("stylish-chairs")} className="hover:bg-slate-200">Stylish Chairs</Button>
@@ -63,7 +63,7 @@ export default  function Gallery() {
 
                         {/* 4th Image */}
 
-                        <div className="w-full lg:w-[240px]  h-[240px] bg-[url('/sanity/4.png')] bg-no-repeat bg-cover bg-center ">
+                        <div className="w-full lg:w-[240px]  h-[240px] bg-[url('/sanity/4-min.png')] bg-no-repeat bg-cover bg-center ">
                             <div className="mt-[171px] ml-[14px]">
                                 <Button  variant={"categoryButton"} onClick={() => handleClick("vintage")}className="hover:bg-slate-200" >Vintage</Button>
                             </div>
